ca77de07edb3b470e20fc8bba6c7598a4dc44558
Sergio Moreno <sergiomoreno@realtracksystems.com>
2020-11-26 22:26:14 +0100

    added user auth

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,5 +1,6 @@
 'use strict'
 const adminCheck = use("App/Middleware/CheckForAdminHeader")
+const userCheck = use("App/Middleware/CheckForUserHeader")
 const User = use('App/Models/User')
 
 class UserController {
@@ -21,28 +22,40 @@ class UserController {
         }
     }
     async show ({ request, response, params }) {
-        response.json(await User.find(params.id));
+        const user = await User.find(params.id)
+        userCheck(user, request, response, () => {
+            response.json(user);
+        })
     }
     async update ({ request, response, params }) {
         var user = await User.find(params.id);
-        user.merge(request.all());
-        await user.save();
-        response.json(user);
+        
+        userCheck(user, request, response, async () => {
+            user.merge(request.all());
+            await user.save();
+            response.json(user);
+        })
     }
     async destroy ({ request, response, params }) {
         var user = await User.find(params.id);
-        response.send(await user.delete());
+        userCheck(user, request, response, async () => {
+            response.send(await user.delete());
+        })
     }
     async getTokens ({ request, response, params }) {
-        
         var user = await User.find(params.id);
         const tokens = await user.tokens().fetch();
-        response.send(tokens);
+        userCheck(user, request, response, () => {
+            response.send(tokens);
+        })
     }
     async getOrders ({ request, response, params }) {
         var user = await User.find(params.id);
         const orders = await user.orders().fetch();
-        response.send(orders);
+        
+        userCheck(user, request, response, () => {
+            response.send(orders);
+        })
     }
 }
 
--- app/Middleware/CheckForUserHeader.js
+++ app/Middleware/CheckForUserHeader.js
@@ -1,7 +1,7 @@
 
 module.exports = function checkForUserHeader(user, request, response, cb) {
 
-    if(request.headers("authorization") == user.id) {
+    if(request.headers().authorization == user.id) {
         cb()
     } else {
         response.status(403).send("Are you lost little buddy? This are not your stuff...")
